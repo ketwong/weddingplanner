74893364497d55aea1935185c56df006b2dc5d1b
ketwong <42503376+ketwong@users.noreply.github.com>
2023-03-11 20:45:35 +0000

    test

--- update_readme.py
+++ update_readme.py
@@ -1,28 +1,28 @@
 import pandas as pd
+import re
 
-# Read the existing README file
+# Read the current README contents
 with open('README.md', 'r') as file:
     contents = file.read()
 
-# Define the new data
-data = [
-    ['💄💅', 'Hair and Makeup', '[Bridebook](https://bridebook.co.uk/), [Hitched](https://www.hitched.co.uk/), [WeddingWire](https://www.weddingwire.co.uk/), [The Knot](https://www.theknot.com/marketplace/wedding-beauty-services)', '£200 - £500+'],
-    ['🎁💍', 'Wedding Favors', '[Bridebook](https://bridebook.co.uk/), [Hitched](https://www.hitched.co.uk/), [WeddingWire](https://www.weddingwire.co.uk/), [Not on the High Street](https://www.notonthehighstreet.com/weddings/wedding-favours)', '£100 - £500+'],
-    ['🏨🛏️', 'Accommodation for Out-of-Town Guests', '[Booking.com](https://www.booking.com/), [Expedia](https://www.expedia.co.uk/), [Hitched](https://www.hitched.co.uk/), [Hotels.com](https://uk.hotels.com/)', '£500 - £2,000+'],
-    ['📋💼', 'Wedding Planner or Coordinator', '[Bridebook](https://bridebook.co.uk/), [Hitched](https://www.hitched.co.uk/), [WeddingWire](https://www.weddingwire.co.uk/), [The Knot](https://www.theknot.com/marketplace/wedding-planners)', '£1,000 - £5,000+']
-]
-
-# Create a DataFrame from the data
-df = pd.DataFrame(data, columns=['Emoji', 'Element', 'Links', 'Estimated Cost'])
-
-# Convert the DataFrame to a markdown table
-table = df.to_markdown(index=False)
-
-# Replace the existing table in the README file with the new table
+# Find the start and end of the table in the README
 start = contents.index('| Element | Links | Estimated Cost |\n')
 end = contents.index('|---|---|---|\n', start)
-new_contents = contents[:start] + table + contents[end:]
 
-# Write the updated contents back to the README file
+# Extract the table from the README and convert to a Pandas DataFrame
+table = contents[start:end]
+df = pd.read_table(pd.compat.StringIO(table), sep='|', index_col=False)
+
+# Add a new row to the DataFrame
+new_row = {'Emoji': '🎉', 'Element': 'New Element', 'Links': '[Link](https://example.com/)', 'Estimated Cost': '£1,000 - £10,000+'}
+df = df.append(new_row, ignore_index=True)
+
+# Convert the DataFrame back to a Markdown table
+table = df.to_markdown(index=False)
+
+# Replace the old table with the updated table in the README
+contents = contents[:start] + table + contents[end:]
+
+# Write the updated contents back to the README
 with open('README.md', 'w') as file:
-    file.write(new_contents)
+    file.write(contents)
